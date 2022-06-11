4e87782345987b5f20d051c3479becce592377ea
Himanshu-Negi-Dev <himanshu.dev7350@gmail.com>
2022-06-11 22:38:38 +0530

    jobs api auth complete

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,13 +1,38 @@
+const bcrypt = require('bcryptjs');
+const { CustomErrorAPI } = require("../errors/CustomErrorAPI");
 const { asyncWrapper } = require("../middleware/async");
+const User = require('../models/user');
 
 const login = asyncWrapper(async (req, res) => {
-  res.send('hello login');
-}) 
+  const { email, password } = req.body;
+  if(!email || !password)  throw new CustomErrorAPI("please enter email & passowrd", 401);
+
+  const user = await User.findOne({ email });
+  if(!user) throw new CustomErrorAPI("User not exist!!", 401);
+
+  const isMatch = await user.comparePassword(password);
+  if(!isMatch) throw new CustomErrorAPI("Invalid Credentials", 401);
+
+  const token = user.createJWT();
+  res.status(200).json({token});
+
+})
 
 
-const register = asyncWrapper((req, res) => {
-  res.send('hello redister')
-}); 
+const register = asyncWrapper(async (req, res) => {
+  const { name, email, password } = req.body;
+  if (!name || !email || !password) throw new CustomErrorAPI("Invalid request", 400);
+
+  let user = await User.findOne({ email });
+  if (user) throw new CustomErrorAPI(`${email} is already used please use diffrent email`, 401);
+  
+  user = { name, email, password }
+
+  const newUser = await User.create(user);
+  const token = newUser.createJWT();
+  res.status(200).json({ token })
+
+});
 
 
 module.exports = { login, register }
---
+++ models/user.js
@@ -0,0 +1,45 @@
+const mongoose = require('mongoose');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'name field is required'],
+    trim: true
+  },
+
+  email: {
+    type: String,
+    required: [true, 'email field is required'],
+    trim: true,
+    unique: true
+  },
+
+  password: {
+    type: String,
+    required: [true, 'This field is required'],
+  }
+});
+
+userSchema.pre('save', async function(){
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+})
+
+userSchema.methods.createJWT = function () {
+  const user = {
+    name: this.name,
+    email: this.email,
+  }
+  return jwt.sign(user, process.env.JWT_SECRET, { expiresIn: "1d" });
+}
+
+userSchema.methods.comparePassword = async function(candidatePassword){
+  const isMatch = await bcrypt.compare(candidatePassword, this.password);
+  // console.log(isMatch)
+  return isMatch;
+}
+
+const User = mongoose.model('User', userSchema);
+module.exports = User;
